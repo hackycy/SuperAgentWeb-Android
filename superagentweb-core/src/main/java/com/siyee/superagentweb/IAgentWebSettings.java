d34178419a3e70c2d51fbb1d8a7923475c155d93
hackycy <qa894178522@qq.com>
2020-08-05 17:39:54 +0800

    fix：remove generic

--- superagentweb-core/src/main/java/com/siyee/superagentweb/IAgentWebSettings.java
+++ superagentweb-core/src/main/java/com/siyee/superagentweb/IAgentWebSettings.java
@@ -3,10 +3,10 @@ package com.siyee.superagentweb;
 import android.webkit.WebSettings;
 import android.webkit.WebView;
 
-public interface IAgentWebSettings<WS extends WebSettings> {
+public interface IAgentWebSettings {
 
     IAgentWebSettings toSetting(WebView webView);
 
-    WS getWebSettings();
+    WebSettings getWebSettings();
 
 }
