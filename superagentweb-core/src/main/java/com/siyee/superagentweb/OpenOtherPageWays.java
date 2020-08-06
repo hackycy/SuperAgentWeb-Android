e0f0c5e49e7da7d739b0784cf6f6d543e1bcd7bd
hackycy <qa894178522@qq.com>
2020-08-06 11:04:31 +0800

    feature：add OpenOtherPageWays enum

---
+++ superagentweb-core/src/main/java/com/siyee/superagentweb/OpenOtherPageWays.java
@@ -0,0 +1,23 @@
+package com.siyee.superagentweb;
+
+/**
+ * @author hackycy
+ */
+public enum OpenOtherPageWays {
+
+    /**
+     * 直接打开跳转页
+     */
+    DERECT,
+
+    /**
+     * 咨询用户是否打开
+     */
+    ASK,
+
+    /**
+     * 禁止打开其他页面
+     */
+    DISALLOW
+
+}
