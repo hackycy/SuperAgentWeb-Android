20ca691b9c493458b291719852f9441da13fcfc7
hackycy <qa894178522@qq.com>
2020-08-12 18:43:42 +0800

    docs：update sample

--- sample/src/main/java/com/siyee/superagentweb/sample/MainActivity.kt
+++ sample/src/main/java/com/siyee/superagentweb/sample/MainActivity.kt
@@ -2,10 +2,26 @@ package com.siyee.superagentweb.sample
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.FrameLayout
+import com.siyee.superagentweb.SuperAgentWeb
 
 class MainActivity : AppCompatActivity() {
+
+    val fl: FrameLayout by lazy  {
+        findViewById<FrameLayout>(R.id.parent)
+    }
+
+    val agentweb: SuperAgentWeb by lazy {
+        SuperAgentWeb.with(this)
+            .setAgentWebParent(fl, FrameLayout.LayoutParams(FrameLayout.LayoutParams.MATCH_PARENT, FrameLayout.LayoutParams.MATCH_PARENT))
+            .build()
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        agentweb.webCreator.webView.loadUrl("https://m.baidu.com")
     }
+
+
 }
