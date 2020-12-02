aec6d9da5f12b80d73a7802dd93587a41601fbb9
hackycy <qa894178522@qq.com>
2020-12-02 15:37:36 +0800

    test：test bridge

--- sample/src/main/java/com/siyee/superagentweb/sample/activity/MainActivity.kt
+++ sample/src/main/java/com/siyee/superagentweb/sample/activity/MainActivity.kt
@@ -3,9 +3,12 @@ package com.siyee.superagentweb.sample.activity
 import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.text.TextUtils
+import android.util.Log
 import android.view.KeyEvent
 import android.widget.FrameLayout
 import com.siyee.superagentweb.SuperAgentWeb
+import com.siyee.superagentweb.bridge.IExecutorFactory
 import com.siyee.superagentweb.sample.R
 
 class MainActivity : AppCompatActivity() {
@@ -19,13 +22,23 @@ class MainActivity : AppCompatActivity() {
             .setAgentWebParent(fl, FrameLayout.LayoutParams(FrameLayout.LayoutParams.MATCH_PARENT, FrameLayout.LayoutParams.MATCH_PARENT))
             .useDefaultIndicator(Color.BLACK)
             .interceptUnkownUrl()
+            .setExecutorFactory(IExecutorFactory { url, func, paramString, _ ->
+                if (func == "log") {
+                    Log.e("SuperAgentWeb", if (TextUtils.isEmpty(paramString)) "nothing" else paramString)
+                    return@IExecutorFactory "{ \"msg\": 2 }"
+                } else {
+                    Log.e("SuperAgentWeb", "nothong to log func : $func url : $url")
+                    return@IExecutorFactory ""
+                }
+
+            })
             .build()
     }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        agentweb.webCreator.webView.loadUrl("https://m.baidu.com")
+        agentweb.go("file:///android_asset/bridge-test.html")
     }
 
     override fun onKeyDown(keyCode: Int, event: KeyEvent?): Boolean {
