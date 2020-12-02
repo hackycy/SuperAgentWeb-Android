d43ca78525b7ae265a10996324fc3cba594e6063
hackycy <qa894178522@qq.com>
2020-12-02 09:34:19 +0800

    feat：IExecutorFactory exec增加url参数

--- superagentweb/src/main/java/com/siyee/superagentweb/bridge/InternalBridge.java
+++ superagentweb/src/main/java/com/siyee/superagentweb/bridge/InternalBridge.java
@@ -1,7 +1,10 @@
 package com.siyee.superagentweb.bridge;
 
 import android.webkit.JavascriptInterface;
+import android.webkit.WebView;
+
 import androidx.annotation.Keep;
+import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 
 /**
@@ -10,16 +13,18 @@ import androidx.annotation.Nullable;
 public class InternalBridge {
 
     private IExecutorFactory mFactory;
+    private WebView mWebView;
 
-    public InternalBridge(@Nullable IExecutorFactory factory) {
+    public InternalBridge(@Nullable IExecutorFactory factory, @NonNull WebView webView) {
         this.mFactory = factory;
+        this.mWebView = webView;
     }
 
     @Keep
     @JavascriptInterface
     public String invoke(String func, String paramString, int callBackId) {
         if (mFactory != null) {
-            return mFactory.exec(func, paramString, callBackId);
+            return mFactory.exec(this.mWebView.getUrl(), func, paramString, callBackId);
         }
         return null;
     }
